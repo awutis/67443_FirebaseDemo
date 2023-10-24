5ef56ad56b3c3adec0296ec229a01b69a6114328
Annie Wu <anniew@andrew.cmu.edu>
2023-10-24 12:33:04 -0400

    Initial Commit

---
+++ FirebaseDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseDemo
+//
+//  Created by Annie Wu on 2023-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseDemo/FirebaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemoApp.swift
+//  FirebaseDemo
+//
+//  Created by Annie Wu on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
